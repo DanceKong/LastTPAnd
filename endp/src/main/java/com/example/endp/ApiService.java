85aa28a861dce8e976e57e7140981ff4d4c164fa
ithing2002 <ithing2002@naver.com>
2023-07-05 21:02:36 +0900

    aa

--- endp/src/main/java/com/example/endp/ApiService.java
+++ endp/src/main/java/com/example/endp/ApiService.java
@@ -1,10 +1,17 @@
 package com.example.endp;
 
+
+
 import java.util.List;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ApiService {
-    @GET("/user")
-    Call<List<User>> getUsers();
+    @POST("test/login")
+//    Call<User> login( @Query("username") String username,
+//                      @Query("password") String password);
+    Call<User> login( @Body LoginRequest loginRequest);
 }
